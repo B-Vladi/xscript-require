216fbf17ea3150f774edf781e49b52a69cc17037
B-Vladi <b-vladi@cs-console.ru>
2013-09-04 16:25:30 +0400

    Update doc

--- doc/scripts/linenumber.js
+++ doc/scripts/linenumber.js
@@ -1,4 +1,4 @@
-(function () {
+(function() {
     var counter = 0;
     var numbered;
     var source = document.getElementsByClassName('prettyprint source');
@@ -7,7 +7,7 @@
         source = source[0].getElementsByTagName('code')[0];
 
         numbered = source.innerHTML.split('\n');
-        numbered = numbered.map(function (item) {
+        numbered = numbered.map(function(item) {
             counter++;
             return '<span id="line' + counter + '" class="line"></span>' + item;
         });
